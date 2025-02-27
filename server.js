05b8ccf625667fa4de160052f4a592ba23cdf0f5
Banele-eng <bmahlalela07@gmail.com>
2025-02-27 17:12:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const { check, validationResult } = require('express-validator');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 
 // Middleware
 app.use(cors());
